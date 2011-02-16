1f360ad10abd7db8d3630ac21d79d6093079ee74
Nathan Vander Wilt <nate@andyet.net>
2011-02-15 17:50:33 -0800

    we haz a queue

--- _attachments/Q.node.js
+++ _attachments/Q.node.js
@@ -4,27 +4,37 @@
 
 // queue primitives
 var Q_TYPE = "net.stemstorage.queue-item";
-function putItem(db, id, item, asyncReturn) {
+function putItem(db, id, value, asyncReturn) {
     var doc = {};
     doc[Q_TYPE] = true;
     doc.timestamp = (new Date).toJSON();
-    doc.item = item;
+    doc.value = value;
     db.http("PUT", doc, id, null, function (status, response) {
         asyncReturn(status === 201, response);
     });
 }
-
+function deleteItem(db, id, rev, asyncReturn) {
+    db.http("DELETE", null, id, {'rev':rev}, function (status, response) {
+        asyncReturn(status === 200, response);
+    });
+}
 function getItems(db, num_desired, item_timeout, respond) {
-    var items = [];
-    function gather(params, yield) {
+    function gather(params, returnCount, yieldItem) {
         db.get("_all_docs", {include_docs:true, skip:params.skip, limit:params.limit}, function (response) {
-            response.rows.filter(function (r) { return r.doc[Q_TYPE]; }).forEach(function (row) {
+            returnCount(response.rows.length);
+            if (!response.rows.length) {
+                yieldItem(null);
+            }
+            response.rows.forEach(function (row) {
                 var doc = row.doc;
+                if (!doc[Q_TYPE]) {
+                    return yieldItem(null);
+                }
+                
                 if (doc.locked_until) {
                     var timeNow = (new Date).toJSON();
-                    console.log(timeNow, doc.locked_until, timeNow < doc.locked_until);
                     if (timeNow < doc.locked_until) {
-                        return;
+                        return yieldItem(null);
                     }
                 }
                 
@@ -32,35 +42,35 @@ function getItems(db, num_desired, item_timeout, respond) {
                 db.http("PUT", doc, doc._id, null, function (status, response) {
                     if (status === 201) {
                         doc._rev = response.rev;
-                        yield(doc);
+                        yieldItem({ticket:JSON.stringify([doc._id, doc._rev]), value:doc.value});
                     }
                 });
             });
         });
     }
-    function send() {
-        respond({json:{items:items}});
-    }
     
-    var attempts = 0, skip = 0, retry;
+    var items = [];
+    var deadline = Date.now() + 250;    // gather items for a quarter second tops
+    var limit = num_desired, skip = 0;
     function attempt() {
-        // TODO: retry only after all docs have been attempted, and don't read off end of _all_docs
-        attempts += 1;
-        console.log("Attempt", attempts);
-        if (attempts < 5) {     // gather items for half a second tops
-            retry = setTimeout(arguments.callee, 100);
-            var limit = num_desired;
-            gather({limit:limit, skip:skip}, function (item) {
+        var remainingItems, fetchCount;
+        gather({limit:limit, skip:skip}, function (count) { remainingItems = fetchCount = count; }, function (item) {
+            remainingItems -= 1;
+            if (item) {
                 items.push(item);
-                if (items.length == num_desired) {
-                    clearTimeout(retry);
-                    send();
+            }
+            
+            if (remainingItems < 1) {
+                limit = num_desired - items.length;
+                if (fetchCount == num_desired && limit && Date.now() < deadline) {
+                    console.log("RETRY on", num_desired, "items, skip now", skip);
+                    process.nextTick(attempt);
+                } else {
+                    respond({json:{items:items}});
                 }
-            });
-            skip += limit;
-        } else {
-            send();
-        }
+            }
+        });
+        skip += limit;
     }
     attempt();
 }
@@ -74,15 +84,41 @@ couch.External2(function (req, respond) {
     //return respond({body:"<h1>Hello World!</h1>\n<pre>\n" + JSON.stringify(req, null, 4) + "</pre>"});
     
     if (req.path.indexOf("favicon.ico") !== -1) {
-        respond({code:404, body:"Go away silly browser.\n"});
+        respond({code:404, body:"What a daft browser you really are!\n"});
         return;
     }
     
     if (req.method === "GET") {
-        getItems(db, parseInt(req.query.count || 1), parseFloat(req.query.timeout || 10.0), respond);
+        getItems(db, parseInt(req.query.count || 1), parseFloat(req.query.time || 10.0), respond);
+    } else if (req.method === "DELETE") {
+        var ticket;
+        try {
+            ticket = JSON.parse(req.body);
+        } catch (e) {
+            respond({code:400, body:"I'm sorry, but that sort of language simply will not do.\n"});
+            return;
+        }
+        deleteItem(db, ticket[0], ticket[1], function (deleted) {
+            if (deleted) {
+                respond({body:"Well done, sir!\n"});
+            } else {
+                respond({code:409, body:"You may have let me know in a more timely fashion.\n"});
+            }
+        });
     } else if (req.method === "POST") {
-        putItem(db, req.uuid, {}, function () {
-            respond({body:"It shall be done.\n"});
+        var value;
+        try {
+            value = JSON.parse(req.body);
+        } catch (e) {
+            respond({code:400, body:"That's not all the Queen's English now, is it?\n"});
+            return;
+        }
+        putItem(db, req.uuid, value, function (added) {
+            if (added) {
+                respond({body:"It shall be done.\n"});
+            } else {
+                respond({code:409, body:"Dear me!\n"});
+            }
         });
     }
     
